8640fa6b8c1e9b05b26ca34ddc6defe13ade47a4
kurizma <foriamkuri@gmail.com>
2026-01-10 20:40:47 +0200

    changes in the product-service for exclusion/coverage

--- backend/product-service/src/main/java/com/buyone/productservice/config/SecurityConfig.java
+++ backend/product-service/src/main/java/com/buyone/productservice/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.buyone.productservice.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
